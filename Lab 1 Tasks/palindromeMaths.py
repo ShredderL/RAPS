83052f2cfbd87aef0db5a45ddc10736298de0ed1
ShredderL <147624989+ShredderL@users.noreply.github.com>
2026-01-29 19:40:32 +0000

    Finished and improved palindrome

--- Lab 1 Tasks/palindromeMaths.py
+++ Lab 1 Tasks/palindromeMaths.py
@@ -1,18 +1,26 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         #if the number is negative, or a multiple of 10 but not 0
-        if x < 0 or x % 10 == 0 and x !=0:
+        if x < 0 or (x % 10 == 0 and x !=0):
             return False
         
         revHalf = 0
         while x > revHalf:
-            
+            revHalf = revHalf * 10 + x % 10 
+            x //= 10
+        return x == revHalf or x == revHalf // 10
 
-            return True
-        return True
-#input
-num = input("Enter number:")
-x = int(num)
-sol = Solution()
-result = sol.isPalindrome(x)
-print(result)
+
+
+while True:
+    #input
+    num = input("Enter number or 'stop':")
+    if num == "stop":
+        break
+    try:
+        x = int(num)
+        sol = Solution()
+        result = sol.isPalindrome(x)
+        print(result)
+    except ValueError:
+        print("Please enter a valid integer.")
