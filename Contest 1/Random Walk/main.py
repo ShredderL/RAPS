62a216b443b7f901fa6c493f05588416fe6992cc
ShredderL <147624989+ShredderL@users.noreply.github.com>
2026-01-30 11:34:55 +0000

    Progress rndm Walk

---
+++ Contest 1/Random Walk/main.py
@@ -0,0 +1,11 @@
+def rndWalk(wlk: str) -> bool:
+        for char in wlk:
+                alph = list[a,b]
+                print(char)
+
+        return True
+
+
+#input
+wlk = str(input("Please enter a string: "))
+print(f"String is: {wlk}")
