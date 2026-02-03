56b00eed6e564bc3560636f78cb163a8934d5275
ShredderL <147624989+ShredderL@users.noreply.github.com>
2026-02-03 00:40:49 +0000

    Completed extended hamming distance

---
+++ Contest 2/Extended Hamming Distance/main.py
@@ -0,0 +1,33 @@
+def HammingDistance(A, B) -> int:
+    numDC = 0
+
+    if len(A) > 50 or len(B) > 50:
+        return ValueError("String length too long.")
+    
+    diff = abs(len(A)-len(B))
+
+    if len(A) > len(B):
+        B += "!" * diff
+    elif len(B) > len(A):
+        A += "!" * diff
+    
+    for i in range(len(A)):
+        if A[i] != B[i]:
+            numDC += 1
+
+
+    return numDC
+
+
+
+
+#input
+first_multiple_input = input("Enter strings: ").rstrip().split()
+
+A = first_multiple_input[0]
+
+B = first_multiple_input[1]
+
+distance = HammingDistance(A, B)
+
+print(distance)
