c57931d07a5fa2ad487a971e1bf5f2726fc4cd27
ShredderL <147624989+ShredderL@users.noreply.github.com>
2026-02-17 13:21:28 +0000

    Finished snake to camel case?

--- Contest 4/snake_case_toCamelCase.py
+++ Contest 4/snake_case_toCamelCase.py
@@ -13,13 +13,15 @@ def SnakeToCamel(snake) -> str:
     newString = newString.replace("_", " ")
     newString = newString.title()
     varName = list(newString.split())
-    varName[0].lower()
-    print(varName, leadingUnderscore)
+    varName[0] = varName[0].lower()
 
+    for i in range(leadingUnderscore):
+        varName.insert(i, "_")
 
+    result = "".join(str(x) for x in varName)
+    print("Variable name in camel case is: ", result)
 
-
-    return varName
+    return result
 
 
 
