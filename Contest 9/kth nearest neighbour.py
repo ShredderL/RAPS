bd4753021da99274185f091f871a3523123e9634
ShredderL <147624989+ShredderL@users.noreply.github.com>
2026-04-15 13:14:21 +0100

    kth nearest neighbour

--- Contest 9/kth nearest neighbour.py
+++ Contest 9/kth nearest neighbour.py
@@ -1,10 +1,18 @@
 
 def KNearestNeighbour(n, k, points):
-    distances = {}
+    distances = []
+    x0 = points[0][0]
+    y0 = points[0][1]
 
+    for i in range(1, n):
+        x = points[i][0]
+        y = points[i][1]
+        distance = (x - x0)**2 + (y - y0)**2
+        distances.append((distance, i))
 
+    distances.sort()
 
-    pass
+    return distances[k-1][1]
 
 
 #input
